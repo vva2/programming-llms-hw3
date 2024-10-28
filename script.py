d7f1da636f10b50abdcf3b38176c57b975222f05
vva <vishaladepu007@gmail.com>
2024-10-28 01:47:01 -0500

    added code for chatbot tutorial using agents.

--- script.py
+++ script.py
@@ -1,87 +1,39 @@
-import os
 from pprint import pprint
+from typing import Optional
 
-from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
-from langchain_core.messages import HumanMessage, AIMessage
-from langchain_core.chat_history import (
-    BaseChatMessageHistory,
-    InMemoryChatMessageHistory,
-)
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import SystemMessage, trim_messages
-from operator import itemgetter
-
-from langchain_core.runnables import RunnablePassthrough
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
 
 
-load_dotenv()
+@tool
+def send_email(recipient_email: Optional[str] = None, subject: Optional[str] = None, body: Optional[str] = None):
+    """sends an email to the 'recipient_email' with the information like subject and body inferred from the user prompt. Note that if the any information is unclear this function should not run."""
+    print(f'sending email to {recipient_email}')
+    print(f'subject:\n {subject}')
+    print(f'body:\n {body}')
 
-model = ChatAnthropic(model="claude-3-5-sonnet-20240620", temperature=0)
-
-store = {}
-
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in store:
-        store[session_id] = InMemoryChatMessageHistory()
-    return store[session_id]
+    return "email send successfully"
 
 
 if __name__ == '__main__':
-    trimmer = trim_messages(
-        max_tokens=40,
-        strategy="last",
-        token_counter=model,
-        include_system=True,
-        allow_partial=False,
-        start_on="human",
-    )
+    load_dotenv()
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You are a helpful assistant. Answer all questions to the best of your ability. You should reply in {language}",
-            ),
-            MessagesPlaceholder(variable_name="messages"),
-        ]
-    )
+    memory = MemorySaver()
+    model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
+    search = TavilySearchResults(max_results=1)
+    tools = [search, send_email]
+    model_with_tools = create_react_agent(model, tools, checkpointer=memory)
 
-    chain = (
-            RunnablePassthrough.assign(messages=itemgetter("messages") | trimmer)
-            | prompt
-            | model
-    )
+    config = {"configurable": {"thread_id": "abc123"}}
 
-    with_message_history = RunnableWithMessageHistory(chain, get_session_history, input_messages_key="messages")
+    response = model_with_tools.invoke({
+        "messages": [HumanMessage(content="send email to vva2@tamu.edu")],
+    }, config=config)
 
-    config = {"configurable": {"session_id": "abc2"}}
-
-    messages = [
-        # SystemMessage(content="you're a good assistant"),
-        HumanMessage(content="hi! I'm bob"),
-        AIMessage(content="hi!"),
-        HumanMessage(content="I like vanilla ice cream"),
-        AIMessage(content="nice"),
-        HumanMessage(content="whats 2 + 2"),
-        AIMessage(content="4"),
-        HumanMessage(content="thanks"),
-        AIMessage(content="no problem!"),
-        HumanMessage(content="having fun?"),
-        AIMessage(content="yes!"),
-    ]
-
-    response = with_message_history.invoke(
-        {
-            "messages": messages + [
-                HumanMessage(content="what math problem did i ask?")
-            ],
-            "language": "english"
-        },
-        config=config
-    )
-
-    pprint(response.content)
-
+    pprint(response['messages'][-1].content)
 
