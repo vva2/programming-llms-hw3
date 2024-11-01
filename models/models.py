9b9d8e8bba46744ad74062845e27c43da06f2a5e
vva <vishaladepu007@gmail.com>
2024-10-31 22:49:38 -0500

    added env flag to run personal assistant using completely local llm

--- models/models.py
+++ models/models.py
@@ -1,10 +1,5 @@
 import os
-
-from dotenv import load_dotenv
-from langchain.agents import create_react_agent
-from langchain.chains import LLMChain
 from langchain_anthropic import ChatAnthropic
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_ollama import ChatOllama
 
 
@@ -15,11 +10,7 @@ class ModelFactory:
 
     local_model = ChatOllama(model=LOCAL_MODEL_NAME, temperature=0)
 
-
-    prompt_template = ChatPromptTemplate([
-        ("system",
-         "You are a helpful assistant with access to various tools. Before using a tool, confirm with the user if it would benefit them. For example, if sending an email, present the draft for approval first. Apply similar consideration to other tools, using your best judgment to ensure the user is informed before any action."),
-        MessagesPlaceholder("messages")
-    ])
-
-    public_model = ChatAnthropic(model_name=PUBLIC_MODEL_NAME, temperature=0)
+    if os.getenv('FULLY_LOCAL') == 1:
+        public_model = local_model
+    else:
+        public_model = ChatAnthropic(model_name=PUBLIC_MODEL_NAME, temperature=0)
--- script.py
+++ script.py
@@ -1,24 +1,19 @@
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
 from Agent import Agent
-
-
 from langchain_core.messages import HumanMessage
 from pydantic import BaseModel, Field
-
 from models.models import ModelFactory
 from utils.loggerr import logger
 
 
-local_model = ModelFactory.local_model
-
-agent = Agent().agent
-
 class PrivateInfoFlag(BaseModel):
     has_private_info: bool = Field(description="true if private data is present, false otherwise")
 
-def can_proceed_safely(user_input: str) -> bool:
+def can_proceed_safely(user_input: str, local_model) -> bool:
     private_data_check_response = local_model.invoke(
         [
             HumanMessage(content=f"Identify the private information in this user prompt. If the private information is not explicitly mentioned then simply dont report anything.:\n-----------`{user_input}`\n-----------")
@@ -27,13 +22,13 @@ def can_proceed_safely(user_input: str) -> bool:
 
     prompt = f"The following response describes the private data found in an user prompt. Using this data answer 'Yes' (if private data is indeed present) or 'No' (otherwise or if you are unsure). BE CONCISE I DO NOT NEED ANY EXPLANATION.\n-----\n{private_data_check_response.content}\n-----"
 
-    result = local_model.invoke([HumanMessage(prompt)])
+    result = ModelFactory.local_model.invoke([HumanMessage(prompt)])
 
     logger.info(f'response: {result} for prompt: {prompt}')
 
     if result and 'yes' in result.content.lower():
         print(f"\n!!PRIVATE INFO FOUND!!:\n{private_data_check_response.content}")
-        user_consent = input("Do you want to proceed sending this info to a public LLM? (y/n): ")
+        user_consent = input("\nDo you want to proceed sending this info to a public LLM? (y/n): ")
 
         if user_consent.lower().strip() != 'y':
             print(f"Your most recent input has been cleared from memory. Please re-phrase it for me.")
@@ -43,13 +38,15 @@ def can_proceed_safely(user_input: str) -> bool:
 
 
 if __name__ == '__main__':
+    agent = Agent().agent
+
     while True:
         try:
             config = {"configurable": {"thread_id": 53}}
             user_input = input("USER>\n")
 
-            # if len(user_input) == 0 or not can_proceed_safely(user_input):
-            #     continue
+            if os.getenv('FULLY_LOCAL') == 0 and len(user_input) == 0 or not can_proceed_safely(user_input, ModelFactory.local_model):
+                continue
 
             response = agent.invoke({"messages": [HumanMessage(content=user_input)]}, config=config)
 
